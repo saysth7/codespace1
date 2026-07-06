40686548ee452509befc11ffd7671093accd1492
saysth7 <saysth7@gmail.com>
2026-07-06 01:49:20 +0000

    tmp save

--- start-10.sh
+++ start-10.sh
@@ -1,10 +1,10 @@
 #cd /workspaces/${GITHUB_REPOSITORY#*/}/win7
 #cd /workspaces/win/win7
-bk=/tmp/sura/disk/windows10.qcow2
+bk=/tmp/win10-sura-my-bk.qcow2
 f=/tmp/win10.qcow2
-#[ -f $f ] && echo "warn: $f exist, backed up." && mv $f $f.bk
+[ -f $f ] && echo "warn: $f exist, backed up." && mv $f $f.bk
 #[ -f $f ] || cp win10-bk.qcow2 $f # github codespace keeps files, so not use git-lfs nor qcow2-inc-snapshot
-#qemu-img create -b $bk -F qcow2 -f qcow2 $f
+qemu-img create -b $bk -F qcow2 -f qcow2 $f
 sudo qemu-system-x86_64 -smp 2 -m 4G -vnc :0 \
  -machine q35 -nic user,model=virtio -usbdevice tablet \
  -monitor stdio \
